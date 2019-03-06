61ded18898a1cf13989fc77baa5d0fa4575c109e
tsunghsuan <tsunghsuanlai@gorilla-technology.com>
2019-03-06 11:28:31 +0800

    [What] 1. Finish slide text animation.

--- app/src/main/java/com/tsunghsuanparty/textanimation/MainActivity.kt
+++ app/src/main/java/com/tsunghsuanparty/textanimation/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.tsunghsuanparty.textanimation
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.tsunghsuanparty.textanimlib.slide.SlideAnimation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        val slowText = findViewById<TextView>(R.id.text_slow)
+        val slideAnim1 = SlideAnimation()
+        slideAnim1.initSettings(Color.BLACK, Color.GREEN, SlideAnimation.ANIM_SLOW)
+        slideAnim1.startAnimation(slowText)
+
+        val normalText = findViewById<TextView>(R.id.text_normal)
+        val slideAnim2 = SlideAnimation()
+        slideAnim2.initSettings(Color.BLACK, Color.GREEN, SlideAnimation.ANIM_NORMAL)
+        slideAnim2.startAnimation(normalText)
+
+        val fastText = findViewById<TextView>(R.id.text_fast)
+        val slideAnim3 = SlideAnimation()
+        slideAnim3.initSettings(Color.BLACK, Color.GREEN, SlideAnimation.ANIM_FAST)
+        slideAnim3.startAnimation(fastText)
     }
 }
---
+++ textanimlib/src/main/java/com/tsunghsuanparty/textanimlib/slide/SlideAnimation.kt
@@ -0,0 +1,77 @@
+package com.tsunghsuanparty.textanimlib.slide
+
+import android.os.Handler
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+
+/**
+ * Author: Tsung Hsuan, Lai
+ * Created on: 2019/3/6
+ * Description:
+ */
+open class SlideAnimation {
+
+    companion object {
+        const val ANIM_SLOW = 0
+        const val ANIM_NORMAL = 1
+        const val ANIM_FAST = 2
+    }
+
+    var mBackColor = 0
+    var mForeColor = 0
+
+    var mAnimSpeed = 0
+
+    fun initSettings(backColor: Int, foreColor: Int, animSpeed: Int) {
+        mBackColor = backColor
+        mForeColor = foreColor
+        mAnimSpeed = animSpeed
+    }
+
+    fun startAnimation(msgTextView: TextView) {
+        val handler = Handler()
+        var startTime: Long
+        var currentTime: Long
+        var finishedTime: Long
+        var endTime: Int
+
+        startTime = System.currentTimeMillis()
+        currentTime = startTime
+
+        handler.postDelayed(object: Runnable {
+            override fun run() {
+                currentTime = System.currentTimeMillis()
+                finishedTime = currentTime - startTime
+
+                when (mAnimSpeed) {
+                    ANIM_SLOW -> endTime = (finishedTime / 280).toInt()
+                    ANIM_NORMAL -> endTime = (finishedTime / 140).toInt()
+                    ANIM_FAST -> endTime = (finishedTime / 70).toInt()
+                    else -> endTime = (finishedTime / 140).toInt()
+                }
+                //LOG.D(TAG, "finished time: $finishedTime, end time: $endTime")
+
+                val spannableString = SpannableString(msgTextView.text)
+
+                // Back to the beginning
+                if (endTime > msgTextView.text.length) {
+                    endTime = 0
+                    startTime = currentTime
+                    spannableString.setSpan(
+                        ForegroundColorSpan(mBackColor), 0,
+                        msgTextView.text.length - 1,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                } else {
+                    spannableString.setSpan(
+                        ForegroundColorSpan(mForeColor), 0, endTime,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
+
+                msgTextView.text = spannableString
+                handler.postDelayed(this, 10)
+            }
+        }, 10)
+    }
+}
